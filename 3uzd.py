a7795f807869bcc724373411c2bd1736337c3451
kurzemnieks-roberts-06 <126758965+kurzemnieks-roberts-06@users.noreply.github.com>
2023-11-09 21:38:11 +0200

    izdarīju 3.uzdevumu

--- 3uzd.py
+++ 3uzd.py
@@ -4,4 +4,10 @@ Dati. Izveidot objektu, kas inicializētu atribūtus,
 piemēram, vārdu, vecumu un ceļojumam iemaksāto summu.
 
 """
+class Dati:
+    def __init__(self, vards, vecums, iem_summa):
+        self.vards=vards
+        self.vecums=vecums
+        self.iem_summa=iem_summa
 
+kkk=Dati("Pēteris", 20, 0.5)
