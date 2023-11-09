3c548234022c5bd0deeb28dd3211f31cd51346d1
kurzemnieks-roberts-06 <126758965+kurzemnieks-roberts-06@users.noreply.github.com>
2023-11-09 21:50:25 +0200

    izpildīju 2.uzd

--- 2uzd.py
+++ 2uzd.py
@@ -2,3 +2,27 @@
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
 """
+
+"""
+iegūst ziņas, izmantojot RSS plūsmu no google.lv.
+
+kas ir RSS?
+
+RSS ir speciāls formāts datu izplatīšanai
+"""
+import feedparser
+
+# URL uz RSS plūsmu
+
+rss_url='https://news.google.com/home?hl=lv&gl=LV&ceid=LV:lv'
+
+#iegūst RSS plūsmas datus
+
+kkk=feedparser.parse(rss_url)
+
+#parāda un noformē
+for entry in kkk.entries:
+    print("Virsrakss:", entry.title)
+    print("Saite: ", entry.link)
+    print('\n')
+
